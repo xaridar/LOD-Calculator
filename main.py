288f72c37537923f7606a84d0009c35080d9cb09
Elliot Topper <elliot.topper@nist.gov>
2024-06-05 13:42:37 -0600

    graph

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from flask import Flask, render_template, jsonify, request, flash, redirect
 from markupsafe import escape
 from werkzeug.utils import secure_filename
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['POST'])
 def upload():
-    if request.method == 'POST':
-        print(request.files)
-        if 'file' not in request.files:
-            print('No file')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            print('No file')
-            return redirect(request.url)
-        filename = secure_filename(file.filename)
-        return jsonify({'name': filename, 'mimetype': file.mimetype, 'params': file.mimetype_params})
+    if 'file' not in request.files:
+        print('No file')
+        return redirect('/')
+    file = request.files['file']
+    if file.filename == '':
+        print('No file')
+        return redirect('/')
+    filename = secure_filename(file.filename)
+    csv_data = pd.read_csv(file.stream)
+    labels = csv_data.x.values.tolist()
+    values = csv_data.y.values.tolist()
+    # for row in reader:
+    #     print(row)
+    # print(contents)
+    return render_template('graph.html', name=filename, labels=labels, values=values)
     
